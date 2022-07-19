0ff57ffc89c04767ce7deba3ae278b7bfe565b4b
Alexandre Mariano <alexandre_mariano@hotmail.com.br>
2022-07-19 15:48:40 -0300

    Atividade Sistema de Multas

---
+++ Aula07/Exercício_10.js
@@ -0,0 +1,13 @@
+let a = {1:['Oi','Tudo bem?']}
+console.log(a[1][0] +', '+ a[1][1]);
+
+let pais = 'Brasil'
+console.log(`Vivendo em ${pais}`);
+if (pais == 'Brasil')
+{
+    console.log('Você é Brasileiro!');
+}
+else
+{
+    console.log(`Você é estrangeiro`);
+}
