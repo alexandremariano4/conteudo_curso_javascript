6523a3eed8049c00621d66ad8db4fb029e3b59b4
Alexandre Mariano <alexandre_mariano@hotmail.com.br>
2022-07-21 18:49:00 -0300

    Reforçando

---
+++ Pratica_01/js/e-commerce.js
@@ -0,0 +1,13 @@
+function cadastrar(){
+    let nome = document.querySelector('input#nome')
+    let desc = document.querySelector('input#desc')
+    let preco = document.querySelector('input#preco')
+    let imagem = document.querySelector('input#imagem')
+    let conteudo = document.createElement('div')
+    let content = document.querySelector('.content')
+    conteudo.setAttribute('id','conteudo')
+    
+    
+
+
+}
